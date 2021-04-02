d6dd0cbb50b06840e186b9a46fd5bf82b7c3e225
JasianE <67132112+JasianE@users.noreply.github.com>
2021-04-02 16:56:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Create array to store book object
 let myLibrary = []
 
-//Instantiate variables
+//Instantiate global variables
 const container = document.getElementById("Wrapper")
 let arrayLength = myLibrary.length;
 let author;
@@ -30,6 +30,8 @@ function Book(title, author, pages, read){
 }
 
 function displayLibrary(){
+        //Create method for changing the read
+        
         //Create container for object information and allows for smepmmcmcmcmcic styling
         const card = document.createElement("div");
         //Instantitate Object properties and info
@@ -45,16 +47,28 @@ function displayLibrary(){
         const author1_text = document.createTextNode("Author: " + author)
         const pages1_text = document.createTextNode("Pages: " + pages)
         const read1_text = document.createTextNode("Read: " + read)
+        change_read.setAttribute('class', 'delete_button')
         delete_button.setAttribute('class', "delete_button")
         delete_button.addEventListener("click", function(){
             container.removeChild(card)
         })
         const button_text = document.createTextNode("Delete Book")
-        change_read.setAttribute('class', 'delete_button')
-        change_read.addEventListener('click', function(){
-            Book.read = prompt("Have you finished reading it yet?")
-        })
-        change_text = document.createTextNode("Change Read Status")
+        const change_read_text = document.createTextNode("Change Read")
+        change_read.addEventListener('click', changeRead)
+
+        function changeRead(){
+            //Obtain value from user
+            let changed_variable = prompt("Have you finished the book?")
+            changed_variable = changed_variable.charAt(0).toUpperCase() + changed_variable.slice(1)
+
+            //Create textnode for read variable
+            let changed_variable_text = document.createTextNode("Read: " + changed_variable)
+
+            //Append and remove previous text
+        
+            read1.innerHTML = '';
+            read1.appendChild(changed_variable_text)
+        }
 
         //Append text to elements
         title1.appendChild(title1_text)
@@ -62,19 +76,17 @@ function displayLibrary(){
         pages1.appendChild(pages1_text)
         read1.appendChild(read1_text)
         delete_button.appendChild(button_text)
-        change_read.appendChild(change_text)
+        change_read.appendChild(change_read_text)
 
         //Append containers to actual container or more professionally main container
         card.appendChild(title1)
         card.appendChild(author1)
         card.appendChild(pages1)
         card.appendChild(read1)
-        card.append(delete_button)
-        card.append(change_read)
+        card.appendChild(delete_button)
+        card.appendChild(change_read)
 
         
 
         container.append(card)
 }
-
-
